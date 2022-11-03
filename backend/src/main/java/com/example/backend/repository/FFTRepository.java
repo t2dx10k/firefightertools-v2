656d00775a1f9a6795f10cbac29616a583e11b92
januszgradzik <ja.gradzik@gmail.com>
2022-11-04 00:00:23 +0100

    Added Dockerfile Fixed FFTRepository.java

--- backend/src/main/java/com/example/backend/repository/FFTRepository.java
+++ backend/src/main/java/com/example/backend/repository/FFTRepository.java
@@ -1,5 +1,6 @@
 package com.example.backend.repository;
 
+import com.example.backend.model.Tool;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FFTRepository extends MongoRepository<Tool, String> {
